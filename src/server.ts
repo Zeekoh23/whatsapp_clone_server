382c35a7b61d6f48f3d556a86d0075f1a9fa10ab
unknown <achueze2922@gmail.com>
2022-02-26 10:44:11 -0800

    heroku5

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,8 @@ import express, { Request, Response } from "express";
 import { app, httpServer } from "./";
 import { AppRouter } from "./AppRouter";
 
+const port: any = process.env.PORT || 5000;
+
 //const DB: any = process.env.DATABASE_LOCAL;
 
 /*mongoose
@@ -40,7 +42,7 @@ app.route("/check").get((req: Request, res: Response) => {
   return res.json("Your app is working fine");
 });
 
-const server = httpServer.listen(4000, "0.0.0.0", () => {
+const server = httpServer.listen(port, "0.0.0.0", () => {
   console.log("app running on port 4000");
 });
 
